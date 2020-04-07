c0346d61477b9023ef2564f0e7ec6ae5dd101bc2
Marshall Dugan <marshall.dugan@gmail.com>
2020-04-06 20:22:04 -0400

    Updates

---
+++ precache-manifest.c726c6d20e0af44fca0b67e7a9f45cec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7002af7482d44de45272b255c0c88d8d",
+    "url": "/Employee-Directory-React/index.html"
+  },
+  {
+    "revision": "ccaba930f77044a1487a",
+    "url": "/Employee-Directory-React/static/css/main.1113edd4.chunk.css"
+  },
+  {
+    "revision": "dd5835f3f72a5bcce278",
+    "url": "/Employee-Directory-React/static/js/2.c7130c28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory-React/static/js/2.c7130c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccaba930f77044a1487a",
+    "url": "/Employee-Directory-React/static/js/main.d107f35c.chunk.js"
+  },
+  {
+    "revision": "211df47ba6c15ea6542d",
+    "url": "/Employee-Directory-React/static/js/runtime-main.94b27ef8.js"
+  }
+]);
